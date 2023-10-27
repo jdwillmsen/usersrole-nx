4fc6248be235c63a76c36524ed564c357a2c1d70
Jake Willmsen <jdwillmsen@gmail.com>
2023-10-27 01:29:55 -0500

    Added component tests to shared ui library

---
+++ libs/shared/ui/src/main/main.component.cy.ts
@@ -0,0 +1,19 @@
+import { TestBed } from '@angular/core/testing';
+import { MainComponent } from './main.component';
+
+describe(MainComponent.name, () => {
+
+  beforeEach(() => {
+    TestBed.overrideComponent(MainComponent, {
+      add: {
+        imports: [],
+        providers: []
+      }
+    })
+  })
+
+  it('renders', () => {
+     cy.mount(MainComponent,);
+  })
+
+})
