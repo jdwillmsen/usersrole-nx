583fef183f96e5c3b04424cb5999625e183b892e
Jake Willmsen <jdwillmsen@gmail.com>
2023-10-28 16:15:30 -0500

    Fixed failing unit tests

--- apps/usersrole-nx/src/app/app.component.spec.ts
+++ apps/usersrole-nx/src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { expect } from '@jest/globals';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -12,6 +13,6 @@ describe('AppComponent', () => {
   it(`should have as title 'usersrole-nx'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('usersrole-nx');
+    expect(app.title).toEqual('Users Role Nx');
   });
 });
